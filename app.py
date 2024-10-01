4d8b626989589da3d6eb3d294784524643c38207
costamay <titusouko@gmail.com>
2024-10-01 10:19:48 +0300

    flask lesson

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_migrate import Migrate
+from models import db
+
+# create a Flask application object
+app = Flask(__name__)
+
+# configure a database connection to the local file app.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdb.db'
+
+# disable modification tracking to use less memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+# create a Migrate object to manage schema modifications
+migrate = Migrate(app, db)
+
+# initialize the Flask application to use the database
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "Welcome to Flask"
+
+@app.route('/users/<username>')
+def getUsername(username):
+    return f'Welcome to flask development {username}'
+
+if __name__ == '__main__':
+    app.run(port=5500, debug=True)
