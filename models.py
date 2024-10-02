cefbf69f1964a7ea283e4f53142f45faf11fb3d2
costamay <titusouko@gmail.com>
2024-10-02 10:26:31 +0300

    one-to-one relationship

--- models.py
+++ models.py
@@ -8,3 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     age = db.Column(db.Integer)
+    
+    profile = db.relationship("Profile", backref="user", uselist=False, cascade="all, delete-orphan")
+    
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    bio = db.Column(db.String(255), nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
--- seed.py
+++ seed.py
@@ -1,16 +1,35 @@
 
 from app import app
-from models import User, db
+from models import User, db, Profile
 
 with app.app_context():
+    # User.query.delete()
+    db.session.query(User).delete()
+    db.session.query(Profile).delete()
+    db.session.commit()
+    
     print("Seeding user data..........")
-    db.session.add(User(username="Felix", age=22))
-    db.session.add(User(username="Brenda", age=19))
-    db.session.add(User(username="Brighton", age=18))
-    db.session.add(User(username="Ivy", age=20))
-    db.session.add(User(username="Chelsea", age=30))
+    
+    # adding users
+    user1 = db.session.add(User(username="Felix", age=22))
+    user2 = db.session.add(User(username="Brenda", age=19))
+    user3 = db.session.add(User(username="Brighton", age=18))
+    user4 = db.session.add(User(username="Ivy", age=20))
+    user5 = db.session.add(User(username="Chelsea", age=30))
+    
+    # Adding profiles
+    profile1 = db.session.add(Profile(bio=" Lovely"))
+    profile2 = db.session.add(Profile(bio="Caring" ))
+    profile3 = db.session.add(Profile(bio="Love dancing"))
+    
+    
+    # giving user profile
+    # profile1.user_id = user1
+    # profile2.user_id = user2
+    # profile3.user_id = user3
     
     db.session.commit()
+
     
     print("Done seeding")
     
